e2f1d085e004974ecac126b0e85b7719485a8c87
Jeovah Fialho <jeovahfialhos@gmail.com>
2024-05-03 13:51:06 -0300

    Insert new comments

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,21 @@
 package main
 
+// Import dependencies that are part of this project.
 import (
-	"raha-beach-code-challenge/internal/itinerary"
-	"raha-beach-code-challenge/pkg/http"
+	"raha-beach-code-challenge/internal/itinerary" // Imports functions and types related to itinerary management.
+	"raha-beach-code-challenge/pkg/http"           // Imports the HTTP server setup utilities.
 )
 
+// The main function is the entry point of the application.
 func main() {
+
+	// Create a new HTTP server using predefined settings from the http package.
 	e := http.NewServer()
+
+	// Register API endpoints related to itineraries, linking them to the server.
 	itinerary.RegisterHandlers(e)
+
+	// Start listening for HTTP requests on port 1323.
+	// If there's an issue starting the server, it will log the error and exit the program.
 	e.Logger.Fatal(e.Start(":1323"))
 }
--- pkg/http/server.go
+++ pkg/http/server.go
@@ -1,16 +1,19 @@
 package http
 
+// Import necessary packages from external libraries.
 import (
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/echo/v4"            // Import the Echo framework for handling HTTP requests.
+	"github.com/labstack/echo/v4/middleware" // Import Echo's built-in middleware for enhanced functionality.
 )
 
+// NewServer function initializes and returns a new Echo server. This server will be used
+// throughout the application to handle HTTP requests.
 func NewServer() *echo.Echo {
-	e := echo.New()
+	e := echo.New() // Create a new instance of an Echo server.
 
 	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())  // Attach Logger middleware which logs all incoming requests.
+	e.Use(middleware.Recover()) // Attach Recover middleware which helps to recover from panics anywhere in the stack chain and handles the HTTP response accordingly.
 
-	return e
+	return e // Return the initialized Echo server instance.
 }
